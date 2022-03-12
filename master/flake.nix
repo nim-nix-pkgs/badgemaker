32045dbb5726686c64cfde3b117e8aae6c84bf9b
hugosenari <hugosenari@gmail.com>
2022-03-12 00:41:36 -0300

    chore: re index badgemaker

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''The Nim badgemaker tool.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-badgemaker-master.flake = false;
+  inputs.src-badgemaker-master.owner = "ethosa";
+  inputs.src-badgemaker-master.ref   = "refs/heads/master";
+  inputs.src-badgemaker-master.repo  = "badgemaker";
+  inputs.src-badgemaker-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-badgemaker-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-badgemaker-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
