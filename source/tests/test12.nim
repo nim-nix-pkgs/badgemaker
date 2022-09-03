2fa780d081a8907cfca3becc26da28ce4290b08b
hugosenari <hugosenari@gmail.com>
2022-09-03 19:46:26 -0300

    feat(nimpkgs): update lock

--- source/tests/test12.nim
+++
@@ -1,6 +0,0 @@
-# author: Ethosa
-# CLI test 1
-import badgemaker/CLI
-import os
-
-parseCommandLine commandLineParams()
--- source/tests/test4.nim
+++
@@ -1,8 +0,0 @@
-# author: Ethosa
-import badgemaker
-
-var badge = newBadge(label="Hello", value="world", style="plastic square",
-                     label_color="#282A36", value_color="fuchsia",
-                     label_text_color="#e0e0e0")
-
-badge.write "test4.svg"
